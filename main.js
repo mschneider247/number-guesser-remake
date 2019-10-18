44d95f61590777ffb9b4d1504d3882eda39f5ebd
Michael Schneider <michaelschneider@Michaels-MBP.PK5001Z>
2019-10-17 22:54:10 -0600

    Add styling and some basic logic

--- main.js
+++ main.js
@@ -2,12 +2,6 @@ var generatedNumber;
 var player1tries = 0;
 var player2tries = 0;
 
-var winner = {
-  title: 'Winner!!',
-  name: '',
-  tries: 1,
-}
-
 var randomNumber = document.querySelector('#randomNumber');
 var minGuess = document.querySelector('#minGuess');
 var maxGuess = document.querySelector('#maxGuess');
@@ -20,26 +14,49 @@ var p1Feedback = document.querySelector('#p1Feedback');
 var p2Feedback = document.querySelector('#p2Feedback');
 var p1tries = document.querySelector('#p1tries');
 var p2tries = document.querySelector('#p2tries');
+var footer = document.querySelector('footer');
+var nameBtn = document.querySelector('#nameBtn');
+var p1NameInput = document.querySelector('#p1Name');
+var p2NameInput = document.querySelector('#p2Name');
 
 rangeBtn.addEventListener('click', setNumber);
 p1Btn.addEventListener('click', attemptP1);
 p2Btn.addEventListener('click', attemptP2);
+nameBtn.addEventListener('click', setNames);
+
+function setNames() {
+  console.log(p1NameInput.value);
+  console.log(p2NameInput.value);
+  document.querySelector('#player1Name').innerText = p1NameInput.value;
+  document.querySelector('#player2Name').innerText = p2NameInput.value;
+  document.querySelector('#player-name-prompt').classList.add('hide')
+}
 
 function setNumber() {
   if ((minGuess.value !== '') && (maxGuess.value !== '')) {
     generatedNumber = parseInt(generateRandomNumber(minGuess.value, maxGuess.value))
     randomNumber.innerText = generatedNumber;
   }
+  document.querySelector('#range').classList.add('hide')
+  document.querySelector('#player-name-prompt').classList.remove('hide')
+  minGuess.value = '';
+  maxGuess.value = '';
 }
 
 function generateRandomNumber(low, high) {
-  randomDecimal = Math.random()
-  return Math.floor((randomDecimal * high) + low);
+  var range = high - low;
+  var randomDecimal = Math.random();
+  var base = (range * randomDecimal);
+  return Math.floor(base) + parseInt(low)
 }
 
 function attemptP1() {
   var p1answer = checkAnswer(player1Guess.value);
   player1tries++;
+  var player1Name = document.querySelector('#player1Name')
+  if (p1answer === 'You Win!') {
+    createWinnerObject(player1Name.innerText, player1tries);
+  }
   p1tries.innerText = player1tries;
   console.log("p1answer:::", p1answer);
   p1Feedback.innerText = p1answer;
@@ -49,6 +66,10 @@ function attemptP1() {
 function attemptP2() {
   var p2answer = checkAnswer(player2Guess.value);
   player2tries++;
+  var player2Name = document.querySelector('#player2Name')
+  if (p2answer === 'You Win!') {
+    createWinnerObject(player2Name.innerText, player2tries);
+  }
   p2tries.innerText = player2tries;
   console.log("p2answer:::", p2answer);
   p2Feedback.innerText = p2answer;
@@ -68,4 +89,19 @@ function checkAnswer(guess) {
   } else {
     return 'Not even trying??'
   }
+}
+
+function createWinnerObject(name, tries) {
+  document.querySelector('#range').classList.remove('hide');
+  generatedNumber = '';
+  randomNumber.innerText = generatedNumber;
+  player1tries = 0;
+  player2tries = 0;
+  footer.insertAdjacentHTML('beforeEnd', `
+      <div class="winnerCard">
+        <p>Winner!</p>
+        <p>${name}</p>
+        <p>tries: ${tries}</p>
+      </div>
+  `)
 }
